4281a522efef46c5e5d6fa6781b45e26129b7108
donny333 <125969039+donny333@users.noreply.github.com>
2023-04-11 10:30:19 +0300

    5 uzduotis.

--- Task 5/script.js
+++ Task 5/script.js
@@ -19,3 +19,17 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+function filterDogOwners(array){
+  let newArray = array.filter(user => user.hasDog);
+  return newArray;
+}
+
+console.log(filterDogOwners(users))
+
+function filterAdults(array){
+  let newArray = array.filter(user => user.age >= 18);
+  return newArray;
+}
+
+console.log(filterAdults(users))
