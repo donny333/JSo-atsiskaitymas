2aa1979c04284fac13d9e33fad0d8eb89a4df209
donny333 <125969039+donny333@users.noreply.github.com>
2023-04-11 08:48:34 +0300

    1 uzduotis.

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,28 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+document
+    .querySelector('.wrapper > form')
+    .addEventListener('submit', event => {
+        event.preventDefault();
+        let svorisKg = Number(event.target.elements.search.value);
+        let svorislb = svorisKg * 2.2046;
+        let svorisg = svorisKg / 0.001;
+        let svorisoz = svorisKg * 35.274;
+
+        let outputDiv = document.querySelector('#output');
+        let lbHeading = document.createElement('h2');
+        let gHeading = document.createElement('h2');
+        let ozHeading = document.createElement('h2');
+        let lbTextNode = document.createTextNode(`${svorisKg}kg = ${svorislb}lb`);
+        let gTextNode = document.createTextNode(`${svorisKg}kg = ${svorisg}g`);
+        let ozTextNode = document.createTextNode(`${svorisKg}kg = ${svorisoz}oz`);
+
+        lbHeading.appendChild(lbTextNode);
+        gHeading.appendChild(gTextNode);
+        ozHeading.appendChild(ozTextNode);
+
+        outputDiv.append(lbHeading, gHeading, ozHeading);
+
+
+});
