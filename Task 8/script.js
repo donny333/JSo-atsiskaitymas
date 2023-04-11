04c222dc92f4bfea325f97e4cdd1011475735d0b
donny333 <125969039+donny333@users.noreply.github.com>
2023-04-11 11:07:34 +0300

    8 uzduotis.

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,25 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
+class Calculator{
+    constructor(no01, no02){
+        this.no01 = no01;
+        this.no02 = no02;
+    }
+    sum(){
+        return this.no01 + this.no02;
+    }
+    subtraction(){
+        return this.no01 - this.no02;
+    }
+    multiplication(){
+        return this.no01 * this.no02;
+    }
+    division(){
+        return this.no01 / this.no02;
+    }
+};
+
+console.log(new Calculator(6, 3).division());
